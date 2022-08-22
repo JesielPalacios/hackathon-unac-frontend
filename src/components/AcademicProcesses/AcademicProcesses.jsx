6108738e0fc3312576e9c2a458163713277aafe8
Jesiel Palacios <jopalacioss@unac.edu.co>
2022-08-22 07:02:16 -0500

    Adds Academic procesess home page (#4)

--- src/components/AcademicProcesses/AcademicProcesses.jsx
+++ src/components/AcademicProcesses/AcademicProcesses.jsx
@@ -1,3 +1,4 @@
+import { Link } from "react-router-dom";
 import styled from "styled-components";
 import { Seo } from "../layout/Seo";
 
@@ -17,6 +18,63 @@ const AcademicProcesses = () => {
         de la Corporaación Universitaria Adventista - UNAC.
       </Subtitle>
 
+      <details>
+        <summary>1. Formulario para solicitar cancelación de materias</summary>
+        <main>
+          <p>
+            <h2>
+              Artículo 42. TÉRMINO PARA CANCELAR LA MATRÍCULA O MATERIAS Y SUS
+              CONSECUENCIAS.
+            </h2>
+
+            <ol>
+              <li>
+                <h3>Cancelación de matrícula</h3>
+                <ul>
+                  <li>
+                    Si la cancelación de matrícula se hace durante los primeros
+                    cinco (5) días de clase, se reembolsará al estudiante el 50%
+                    del dinero que haya pagado, o el total del pago se le dejará
+                    en consignación para una próxima matrícula, de conformidad
+                    con lo dispuesto en el parágrafo único del artículo 41 del
+                    presente reglamento.
+                  </li>
+                  <li>
+                    Si la cancelación se hace después de los primeros cinco (5)
+                    días de clase no se aplicará lo dispuesto en el numeral
+                    anterior.
+                  </li>
+                </ul>
+              </li>
+              <li>
+                <h3>Cancelación de materias</h3>
+                <ul>
+                  <li>
+                    Si la cancelación de materias se realiza después de la fecha
+                    límite establecida para tal efecto, de conformidad con lo
+                    dispuesto en el numeral 2 del artículo 39 del presente
+                    Reglamento, se registrarán las notas que el alumno tenga en
+                    el acumulado porcentual hasta ese momento. Lo que quiere
+                    decir que obtendrá cero punto cero (0.0) en las notas
+                    faltantes.
+                  </li>
+                  <li>
+                    Si la cancelación se hace antes de la fecha límite para
+                    cancelación de materias en los términos definidos para tal
+                    efecto, no se registrarán notas, y el estudiante quedará
+                    como si no hubiese cursado el semestre.
+                  </li>
+                </ul>
+              </li>
+            </ol>
+
+            <LinkRouter to="cancelar-materia">
+              Solicitar cancelación de materia
+            </LinkRouter>
+          </p>
+        </main>
+      </details>
+
       {processes.map((process, index) => {
         return (
           <details key={index}>
@@ -24,55 +82,7 @@ const AcademicProcesses = () => {
               {index + 1}. {process.title}
             </summary>
             <main>
-              {/* <p>{process.description}</p> */}
-              <p>
-                <h2>
-                  Artículo 42. TÉRMINO PARA CANCELAR LA MATRÍCULA O MATERIAS Y
-                  SUS CONSECUENCIAS.
-                </h2>
-
-                <ol>
-                  <li>
-                    <h3>Cancelación de matrícula</h3>
-                    <ul>
-                      <li>
-                        Si la cancelación de matrícula se hace durante los
-                        primeros cinco (5) días de clase, se reembolsará al
-                        estudiante el 50% del dinero que haya pagado, o el total
-                        del pago se le dejará en consignación para una próxima
-                        matrícula, de conformidad con lo dispuesto en el
-                        parágrafo único del artículo 41 del presente reglamento.
-                      </li>
-                      <li>
-                        Si la cancelación se hace después de los primeros cinco
-                        (5) días de clase no se aplicará lo dispuesto en el
-                        numeral anterior.
-                      </li>
-                    </ul>
-                  </li>
-                  <li>
-                    <h3>Cancelación de materias</h3>
-                    <ul>
-                      <li>
-                        Si la cancelación de materias se realiza después de la
-                        fecha límite establecida para tal efecto, de conformidad
-                        con lo dispuesto en el numeral 2 del artículo 39 del
-                        presente Reglamento, se registrarán las notas que el
-                        alumno tenga en el acumulado porcentual hasta ese
-                        momento. Lo que quiere decir que obtendrá cero punto
-                        cero (0.0) en las notas faltantes.
-                      </li>
-                      <li>
-                        b. Si la cancelación se hace antes de la fecha límite
-                        para cancelación de materias en los términos definidos
-                        para tal efecto, no se registrarán notas, y el
-                        estudiante quedará como si no hubiese cursado el
-                        semestre.
-                      </li>
-                    </ul>
-                  </li>
-                </ol>
-              </p>
+              <p>{process.description}</p>
             </main>
           </details>
         );
@@ -85,14 +95,16 @@ export default AcademicProcesses;
 
 const Container = styled.div`
   padding-top: 35px;
-  padding-left: 60px;
-  padding-right: 60px;
+  padding-left: 80px;
+  padding-right: 80px;
   /* border: 1px solid #000; */
   /* background: red; */
   height: 100%;
-
+  
   details {
     margin-bottom: 15px;
+    padding-bottom: 15px;
+    height: 100%;
     &[open] {
       summary {
         color: #ee6b4e;
@@ -142,7 +154,8 @@ const Container = styled.div`
       padding: 0 18px;
       background-color: white;
       /* max-height: 0; */
-      overflow: hidden;
+      /* max-height: fit-content; */
+      /* overflow: hidden; */
     }
   }
 
@@ -153,6 +166,34 @@ const Container = styled.div`
     summary {
       background-color: #ccc;
     }
+  }
+`;
+
+const LinkRouter = styled(Link)`
+  font-family: "Poppins", sans-serif;
+  font-size: 13px;
+  text-decoration: none;
+  color: #000;
+
+  border: none;
+  height: 100%;
+  margin: 0px;
+  border-radius: 0.5rem;
+  padding: 0.5rem 1rem;
+  font-weight: 600;
+  border: 1px solid #f5a800;
+  border: 1px solid #ee6b4e;
+  /* background: #f5f5f5; */
+  /* background: #eeeeee; */
+  background: none;
+  color: #f5a800;
+  color: #ee6b4e;
+
+  :hover {
+    background-color: #f5a800;
+    background-color: #ee6b4e;
+    color: #ee6b4e;
+    color: #ffffff;
   }
 `;
 
@@ -179,7 +220,7 @@ const Subtitle = styled.h2`
 const processes = [
   {
     title: "Formulario para solicitar cancelación de materias.",
-    description: "Hola",
+    description: `Lorem ipsum dolor, sit amet consectetur adipisicing elit. Voluptatibus quibusdam tempore, eius voluptas blanditiis, aperiam doloremque voluptate, saepe delectus aliquam reiciendis hic? Dolor, reiciendis labore rerum non asperiores vitae fugiat?`,
   },
   {
     title: "Formulario para solicitar habilitación de materias.",
